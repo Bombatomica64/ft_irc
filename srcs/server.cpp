63cbe6bfd8c76213419d489eb9e4d931f0c9f1a5
Bombatomica_64 <qualcosa2001@gmail.com>
2024-07-18 13:07:30 +0200

    Refactor Server class to improve code readability and maintainability

--- srcs/server.cpp
+++ srcs/server.cpp
@@ -6,7 +6,7 @@
 /*   By: lmicheli <lmicheli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/25 11:59:47 by lmicheli          #+#    #+#             */
-/*   Updated: 2024/07/18 11:20:19 by lmicheli         ###   ########.fr       */
+/*   Updated: 2024/07/18 12:41:47 by lmicheli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -177,14 +177,6 @@ void Server::accept_connection()
 	}
 }
 
-void Server::read_from_client(int client)
-{
-	if (m_clients[client]->get_registered() == false || m_clients[client]->get_connected() == false)
-	{
-		register_client(client);
-		return;
-	}
-	char buffer[3] = {0};
 	// int ret = recv(client, buffer, BUFFER_SIZE, 0);
 	// if (ret == -1)
 	// {
@@ -196,25 +188,38 @@ void Server::read_from_client(int client)
 	// 	close(client);
 	// 	return;
 	// }
-	int ret;
-	std::string msg = "";
-	while ((ret = recv(client, buffer, 1, 0)) > 0 || (msg.find("\r\n") == std::string::npos)) // check docs for \r\n
+void Server::read_from_client(int client)
+{
+	if (m_clients[client]->get_registered() == false || m_clients[client]->get_connected() == false)
 	{
-		msg.append(buffer, ret);
-		if (msg.find("\r\n") != std::string::npos) // check docs for \r\n
-			break;
+		register_client(client);
+		return;
 	}
-	std::cerr << ret << std::endl;
-	if (ret == -1)
+	char temp[2] = {0};
+	int ret;
+	int total_ret = 0;
+
+	std::string msg = "";
+	while ((ret = recv(client, temp, 1, 0)) > 0 || (msg.find("\r\n") == std::string::npos)) // check docs for \r\n
+	{
+		total_ret += ret;
+		if (msg.find("\r\n") != std::string::npos || ret == -1) // check docs for \r\n
+			break ;
+		
+		msg.append(temp, ret);
+	}
+	std::cerr << total_ret << std::endl;
+	if (total_ret == -1)
 	{
 		std::cout << "Error: mannagia a cristo " << strerror(errno) << std::endl;
 	}
 	if (msg.empty())
 	{
-		std::cerr << "haha, i'm in danger 🚌️🤸️" << std::endl;
+		std::cerr << "🥍haha, i'm in danger 🚌️🤸️" << std::endl;
 		throw Server::ClientException();
 		return;
 	}
+	std::cout << RED "Received: " << msg.substr(0, msg.size() - 1) << RESET << std::endl; 
 	std::vector<std::string> split_msg = split(msg, " ");
 	parse_cmds(client, trimString(msg));
 	// if (m_commands.find(split_msg[0]) == m_commands.end())
@@ -235,28 +240,29 @@ void Server::register_client(int client)
 
 	char temp[2] = {0};
 	int ret;
+	int total_ret = 0;
 
 	std::string msg = "";
 	while ((ret = recv(client, temp, 1, 0)) > 0 || (msg.find("\r\n") == std::string::npos)) // check docs for \r\n
 	{
+		total_ret += ret;
+		if (msg.find("\r\n") != std::string::npos || ret == -1) // check docs for \r\n
+			break ;
+		
 		msg.append(temp, ret);
-		if (msg.find("\r\n") != std::string::npos) // check docs for \r\n
-			break;
 	}
-	// ret = recv(client, temp, BUFFER_SIZE, 0);
-	// msg.erase(msg.find("\r\n"));
-	std::cerr << ret << std::endl;
-	if (ret == -1)
+	std::cerr << total_ret << std::endl;
+	if (total_ret == -1)
 	{
 		std::cout << "Error: mannagia a cristo " << strerror(errno) << std::endl;
 	}
 	if (msg.empty())
 	{
-		std::cerr << "haha, i'm in danger 🚌️🤸️" << std::endl;
+		std::cerr << "🥍haha, i'm in danger 🚌️🤸️" << std::endl;
 		throw Server::ClientException();
 		return;
 	}
-	std::cout << RED "Received: " << msg << RESET << std::endl; // TODO remove
+	std::cout << RED "Received: " << msg.substr(0, msg.size() - 1) << RESET << std::endl; // TODO remove
 	if (msg == "\n")
 	{
 		write_to_client(client, "You must send a password first");
